abbfb17eedb2af7afc7f82d46e00c42db94c54f0
Karan-Bathani <karan.develops@gmail.com>
2022-10-03 15:03:57 -0400

    - Final Commit

--- server/models/product.js
+++ server/models/product.js
@@ -2,39 +2,39 @@ const mongoose = require("mongoose");
 const ratingSchema = require("./rating");
 
 const productSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  images: [
-    {
-      type: String,
-      required: true,
-    },
-  ],
-  quantity: {
-    type: Number,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  category: {
-    type: Number,
-    required: true,
-  },
-  ratings: [ratingSchema],
-  avg_rating: {
-    type: Number,
-  },
-  // add an average rating field that is calculated from the average of all ratings.
+	name: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	description: {
+		type: String,
+		required: true,
+		trim: true,
+	},
+	images: [
+		{
+			type: String,
+			required: true,
+		},
+	],
+	quantity: {
+		type: Number,
+		required: true,
+	},
+	price: {
+		type: Number,
+		required: true,
+	},
+	category: {
+		type: Number,
+		required: true,
+	},
+	ratings: [ratingSchema],
+	// an average rating field that is calculated from the average of all ratings.
+	avg_rating: {
+		type: Number,
+	},
 });
 
 const Product = mongoose.model("Product", productSchema);
